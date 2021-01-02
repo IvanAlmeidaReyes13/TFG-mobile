f7956e1ab92fbd55d689ebc73177916f39b38d2d
IvanAlmeidaReyes13 <55575710+IvanAlmeidaReyes13@users.noreply.github.com>
2021-01-02 12:08:42 +0100

    Update firebase.js

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -5,13 +5,13 @@ import 'firebase/storage'
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDc4qkWCb_73_VqUdJU7iLsH0mpXeUovGc",
-  authDomain: "tfg-ivan-almeida.firebaseapp.com",
-  databaseURL: "https://tfg-ivan-almeida.firebaseio.com",
-  projectId: "tfg-ivan-almeida",
-  storageBucket: "tfg-ivan-almeida.appspot.com",
-  messagingSenderId: "1096393223498",
-  appId: "1:1096393223498:web:9ac8600684a6400b7fa5b3"
+  apiKey: "API?KEY",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
